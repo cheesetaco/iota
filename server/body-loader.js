ffc2ac49cc6ef867745c0ff979be3b42be25a302
kyle <kyletrutland@gmail.com>
2014-03-29 17:19:27 -0700

    renamed variables

--- server/body-loader.js
+++ server/body-loader.js
@@ -1,15 +1,16 @@
+"use strict"
 var	CORE = require('./core.js').CORE;
 
 
 CORE.register('body:loader', function() {
 	return {
 		init : function() {
-			CORE.start('neo/blockIDs')
+			CORE.start('body:loader:blocks/ids')
 			CORE.start('body:loader:response/blocks')
 			CORE.start('body:loader:request/blocks')
 		},
 		destroy : function() {
-			CORE.stop('neo/blockIDs')
+			CORE.stop('body:loader:blocks/ids')
 			CORE.stop('body:loader:response/blocks')
 			CORE.stop('body:loader:request/blocks')
 		}
@@ -20,98 +21,86 @@ CORE.register('body:loader:request/blocks', function(sb) {
 
 	return {
 		init : function() {
-			sb.listen('(router)body/get', this.getBody)
+			sb.listen('(body:loader)blocks/get', this.getParentID)
 		},
 		destroy : function() {
-			sb.ignore('(router)body/get')
+			sb.ignore('(body:loader)blocks/get')
 		},
-		getBody : function(evt) {
-			var reqObj = evt.data,
-				request  = reqObj.req,
-				response = reqObj.res;
-			response.writeHead(200, {"Content-Type": "text/json"})
+		getParentID : function(evt) {
+			var data = evt.data,
+				parentNodes = data.parentNodes;
 
-			request.on('data', function(data) 
-			{
-				var jsonObject = JSON.parse(data),
-					parentNodes = jsonObject.parentNodes;
-
-				if (parentNodes && parentNodes.length > 0)
-				{
-					sb.dispatch('neo/currentNodeID/get', parentNodes)
-				}
-			})			
+			if (parentNodes && parentNodes.length > 0)
+				sb.dispatch('(neo)currentNodeID/get', parentNodes)
 		}
 
 	}
 })
-CORE.register('neo/blockIDs', function(sb) {
+CORE.register('body:loader:blocks/ids', function(sb) {
 
 	return {
 		init : function() {
-			sb.listen('neo/currentNodeID/post', this.getBlocks)
+			sb.listen('(neo)currentNodeID/post', this.getBlocks)
 		},
 		destroy: function() {
-			sb.ignore('neo/currentNodeID/post')
+			sb.ignore('(neo)currentNodeID/post')
 		},
 		getBlocks: function(evt) {
 			var currentNodeID = evt.data,
 				query = 'match (:object {id:"'+currentNodeID+'"})-[:contains]->(n:block) return n.id order by n.sort';
 
-			sb.dispatch('neo/query', {query:query , event:'neo/blockIDs/post'})
+			sb.dispatch('(neo)query', {query:query , event:'(neo)blocks/ids/post'})
 		}
 	}
 })
 CORE.register('body:loader:response/blocks', function(sb) {
-	var reqObj, currentNodeID, realIDlist;
+	var currentNodeID, neoIDs;
 	return {
 		init : function() {
 			sb.listen({
-				'(server)request'			: this.cacheReqObj,
-				'neo/currentNodeID/post' 	: this.cacheCurrentNodeID,
-				'neo/blockIDs/post' 		: this.cacheBlocksIDs,
+				'(neo)currentNodeID/post' 	: this.cacheCurrentNodeID,
+				'(neo)blocks/ids/post' 		: this.cacheNeoIDs,
+				
 				'(sql)blocks/content/post' 	: this.sendBlocks
 			})
 		},
 		destroy : function() {
 			sb.ignore('(sql)blocks/content/post')
 		},
-		cacheReqObj : function(evt) {
-			reqObj = evt.data
-		},
 		cacheCurrentNodeID : function(evt) {
 			currentNodeID = evt.data
 		},
-		cacheBlocksIDs : function(evt) {
-			realIDlist = evt.data
+		cacheNeoIDs : function(evt) {
+			neoIDs = evt.data
 		},
+
 		sendBlocks : function(evt) {
-			var response = reqObj.res,
-				rows = evt.data.rows,
-				blocksArray = [],
-				sqlIDList = [];
+			var sqlRows = evt.data.rows,
 
-			for (i=0;i<rows.length;i++)
-			{
-				var sqlID = rows[i].id
-				sqlIDList.push(sqlID)
-			}
-
-			//display in the right order -- fucking sql
-			for (j=0;j<realIDlist.length;j++)
-			{
-				var num = sqlIDList.indexOf(realIDlist[j]),
-					realContent = rows[num].content; 
-				blocksArray.push(realContent)
-			}
-
-			var sendList = JSON.stringify({
-				blocks 	: 	blocksArray,
-				ids 	: 	realIDlist,
+			send = {
+				blocks 	: 	[],
+				ids 	: 	[],
 				parentID: 	currentNodeID
-			});
+			},
+			neoContent,
+			sqlID, sqlContent, num, i;
 
-			response.end(sendList);			
+			for (i=0 ; i<sqlRows.length ; i++)
+			{
+				sqlID = sqlRows[i].id
+				send.ids.push(sqlID)
+			}
+			//display in the right order -- fucking sql
+			for (i=0 ; i<neoIDs.length ; i++)
+			{
+				neoContent 	= neoIDs[i]
+				num 		= send.ids.indexOf(neoContent)
+				sqlContent 	= sqlRows[num].content 
+				
+				send.blocks.push(sqlContent)
+			}
+
+			sb.dispatch('(router)respond', send);
 		}
 
 	}
